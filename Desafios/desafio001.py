9c6f3603f97c89367239ec9e3b33f36fce938931
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-06 17:40:35 -0300

    Criei script do desafio 001 de python

---
+++ Desafios/desafio001.py
@@ -0,0 +1,6 @@
+'''Crie um script que leia o nome de pessoa
+e mostre uma mensagem de boas-vindas de acordo
+com o valor digitado.'''
+
+nome = input('Qual é oseu nome? ')
+print("Seja bem-vindo" ,nome)
