cdabafcf4707a5ccb0e1c72e5f2e8d97d41c0f33
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-06 17:41:10 -0300

    Criei script do desafio 002 de python

---
+++ Desafios/desafio002.py
@@ -0,0 +1,9 @@
+'''Crie um script que leia p dia, mês e o ano
+de anscimento de uma pessoa e mostre uma
+mensagem com data formatada.'''
+
+dia = input("Dia = ")
+mes = input("Mês = ")
+ano = input("Ano = ")
+
+print("Você nasceu no dia" ,dia ,"de" ,mes ,"de",ano,". Correto?")
