7171bc5ba49a10f15f8c774a07311c0ad3bde839
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-06 17:41:51 -0300

    Criei script do desafio 003 de python

---
+++ Desafios/desafio003.py
@@ -0,0 +1,10 @@
+'''Crie um script que leia dois
+numeros e tente mostrar a
+soma entre eles.'''
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digie outro número: "))
+
+sm = n1 + n2
+
+print("A soma ente ",n1 ,"e",n2 ,"é igual a: ",sm)
