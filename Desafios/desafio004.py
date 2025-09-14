6e0cdfed1531d47a90cd5c313f8673ed0725f59c
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-09-14 19:33:00 -0300

    Criei o desafio 004

---
+++ Desafios/desafio004.py
@@ -0,0 +1,18 @@
+'''Faça um programa queleia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele.'''
+
+n = input('Digite algo: ')
+print('O tipo primitivo desse valor é', type(n))
+print('Só tem espaços?', n.isspace())
+print('É um número?', n.isnumeric())
+print('É alfabético?', n.isalpha())
+print('É alfanumérico?', n.isalnum())
+print('Está em maiúsculas?', n.isupper())
+print('Está em minúsculas?', n.islower())
+print('Está capitalizada?', n.istitle())
+print('É decimal?', n.isdecimal())
+print('É identificador?', n.isidentifier())
+print('É imprimível?', n.isprintable())
+print('Está em caixa baixa?', n.islower())
+print('Está em caixa alta?', n.isupper())
+print('Está em título?', n.istitle())
+print('É um dígito?', n.isdigit())
