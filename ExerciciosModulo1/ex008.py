617cc0e237814f36983b377c5290eb8d11509525
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-04-21 15:20:10 -0300

    Conversão de medidas

---
+++ ExerciciosModulo1/ex008.py
@@ -0,0 +1,8 @@
+m = float(input('Digite uma distância metros: '))
+km = m * 0.001
+hm = m * 0.01
+dm = m * 0.1
+cm = m * 100
+mm = m * 1000
+
+print(f'A distância de {m} m corresponde em:\n Kilometros é de {km} km \n Hectometros é de {hm} hm \n Decametros é de {dm} dm \n Centimetros é de {cm} cm \n Milimetros é de {mm} mm')
