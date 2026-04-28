aa0bfd80fb187c725d1045dfc5edf33b9fef0e13
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-04-28 20:16:29 -0300

    Aumento de salário

---
+++ ExerciciosModulo1/ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual é o salário do Funcionário? R$ '))
+novo_salario = salario + (salario * 15 / 100)
+print(f'Um Funcionário que ganhava R$ {salario:.2f}, com 15 po cento de aumento passa a receber R$ {novo_salario:.2f}')
